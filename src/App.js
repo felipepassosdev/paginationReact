1aebe0f4ea0810cf439c86f40c39cb9cb91a2e6c
felipe.passos <felipe.passos@iv2.com.br>
2019-09-18 17:02:33 -0300

    att

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Header from './componentes/Header'
+import Main from './pages/main'
+import './styles.css'
+
+const App = () => (
+    <div className="App">
+      <Header/>
+      <Main/>
+    </div>
+  );
+
+export default App;
